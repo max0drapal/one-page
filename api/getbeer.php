d8c109259abe3c4ce0364b464021a113a7f49ce9
max0drapal <maxdrapal@outlook.com>
2026-01-16 12:36:21 +0100

    Update getbeer.php

--- api/getbeer.php
+++ api/getbeer.php
@@ -1,1 +1,15 @@
 
+<?php
+header('Content-Type: application/json');
+$action = $_GET['action'] ?? '';
+
+if ($action === 'get_beers') {
+    echo file_exists('data.json') ? file_get_contents('data.json') : json_encode([]);
+    exit;
+}
+
+if ($action === 'save_contact') {
+    echo json_encode(["status" => "success", "message" => "Zpráva odeslána přes AJAX!"]);
+    exit;
+}
+?>
